66ee7d8477d982e3825d176e8f7dcafc9d6c83f6
Alicia <aliciahuang@cpp.edu>
2025-12-04 15:44:23 -0800

    Created tag form

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'recipe_manager.dart';
 import 'package:provider/provider.dart';
+import 'tag_form_page.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -12,50 +13,114 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+
+  late final TextEditingController _tagController;
+
+  @override 
+  void initState() {
+    super.initState();
+    _tagController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _tagController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     int recipeCount = context.watch<RecipeManager>().recipes.length;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8.0),
-            border: Border.all(color: Colors.teal, width: 2.0),
-          ),
-          child: Wrap(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Center(
-                  child: Column(
-                    children: [
-                      Text(
-                        recipeCount.toString(),
-                        style: TextStyle(
-                          fontSize: 48,
-                          fontWeight: FontWeight.bold,
-                        ),  
+        appBar: AppBar(
+          title: const Text('Profile'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  child: Center(
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(color: Colors.teal, width: 2.0),
                       ),
-                      Text(
-                        'Recipes Stored',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      child: Column(
+                        children: [
+                          Text(
+                            recipeCount.toString(),
+                            style: TextStyle(
+                              fontSize: 48,
+                              fontWeight: FontWeight.bold,
+                            ),  
+                          ),
+                          Text(
+                            'Recipes Stored',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ],  
-          ),
-        ), 
-      ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      border: Border.all(color: Colors.teal, width: 2.0),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: Text(
+                            'Manage Your Tags',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: Column(
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context, 
+                                    MaterialPageRoute(
+                                      builder: (context) => const TagFormPage(),
+                                    ),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8.0),
+                                  ),
+                                ),
+                                child: Text('Add New Tag')
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],  
+            ),
+        ),
     );
   }
 }
---
+++ lib/tag_form_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'managers/type_manager.dart';
+
+class TagFormPage extends StatefulWidget {
+  const TagFormPage({Key? key, this.tagName}) : super(key: key);
+
+  final String? tagName;
+
+  @override
+  State<TagFormPage> createState() => _TagFormPageState();
+}
+
+class _TagFormPageState extends State<TagFormPage> {
+
+  late final TextEditingController _tagController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tagController = TextEditingController(text: widget.tagName ?? '');
+  }
+
+  @override
+  void dispose() {
+    _tagController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          primary: Colors.teal
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: const BorderSide(color: Colors.grey, width: 1),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: const BorderSide(color: Colors.teal, width: 3),
+          ),
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Tag Form'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Tag Name',
+                ),
+                controller: _tagController,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  String newTag = _tagController.text.trim();
+                  if (newTag.isNotEmpty) {
+                    Provider.of<TypeManager>(context, listen: false).addType(newTag); 
+                    Navigator.pop(context);
+                  }
+                },
+                child: widget.tagName != null
+                  ? const Text('Update Tag')
+                  : const Text('Add Tag'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+} 
